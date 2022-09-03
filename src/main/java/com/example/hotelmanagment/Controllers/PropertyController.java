dc74013c61a54ce58b48ab156849310b9fb6eb65
satish358 <satishmankar745@gmail.com>
2022-09-03 13:29:44 +0530

    CORS allowed

--- src/main/java/com/example/hotelmanagment/Controllers/PropertyController.java
+++ src/main/java/com/example/hotelmanagment/Controllers/PropertyController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/properties")
 public class PropertyController {
